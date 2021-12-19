29ace9d6da67dd2d98fcf8cae4e83e804c2d6f16
MarinaGryphon <ryfoxx93@gmail.com>
2021-12-19 11:17:14 -0600

    Remove and fix grep tests

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -6,19 +6,23 @@ shopt -s globstar
 
 st=0
 
+echo "Checking for non-TGM formatted maps"
 if grep -El '^\".+\" = \(.+\)' maps/**/*.dmm;	then
     echo "ERROR: Non-TGM formatted map detected. Please convert it using Map Merger!"
     st=1
 fi;
-if grep -P '^\ttag = \"icon' maps/**/*.dmm;	then
-    echo "ERROR: tag vars from icon state generation detected in maps, please remove them."
+echo "Checking for tags in maps"
+if grep -P '^\ttag = ' maps/**/*.dmm;	then
+    echo "ERROR: tag vars detected in maps, please remove them."
     st=1
 fi;
-if grep -P 'step_[xy]' maps/**/*.dmm;	then
+echo "Checking for step_x/step_y in maps"
+if grep -P '^\tstep_[xy]\b' maps/**/*.dmm;	then
     echo "ERROR: step_x/step_y variables detected in maps, please remove them."
     st=1
 fi;
-if grep -P 'pixel_[^xy]' maps/**/*.dmm;	then
+echo "Checking for pixel_w/pixel_z in maps"
+if grep -P '^\tpixel_[wz]\b' maps/**/*.dmm;	then
     echo "ERROR: incorrect pixel offset variables detected in maps, please remove them."
     st=1
 fi;
@@ -27,10 +31,17 @@ if grep -P '/obj/structure/cable(/\w+)+\{' maps/**/*.dmm;	then
     echo "ERROR: vareditted cables detected, please remove them."
     st=1
 fi;
+echo "Checking for stacked cables"
+if grep -P '"\w+" = \(\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/area/.+\)' _maps/**/*.dmm;	then
+    echo "found multiple cables on the same tile, please remove them."
+    st=1
+fi;
+echo "Checking for vareditted /area paths"
 if grep -P '^/area/.+[\{]' maps/**/*.dmm;	then
     echo "ERROR: Vareditted /area path use detected in maps, please replace with proper paths."
     st=1
 fi;
+echo "Checking for base /turf path in maps"
 if grep -P '\W\/turf\s*[,\){]' maps/**/*.dmm; then
     echo "ERROR: base /turf path use detected in maps, please replace with proper paths."
     st=1
@@ -49,6 +60,7 @@ if grep -P '^\t+ [^ *]' code/**/*.dm; then
     echo "mixed <tab><space> indentation detected"
     st=1
 fi;
+echo "Checking for missing trailing newlines"
 nl='
 '
 nl=$'\n'
@@ -59,45 +71,28 @@ while read f; do
         st=1
     fi;
 done < <(find . -type f -name '*.dm')
+echo "Checking for var/ in proc arguments"
 if grep -P '^/[\w/]\S+\(.*(var/|, ?var/.*).*\)' code/**/*.dm; then
     echo "changed files contains proc argument starting with 'var'"
     st=1
 fi;
-if grep -i 'centcomm' code/**/*.dm; then
-    echo "ERROR: Misspelling(s) of CENTCOM detected in code, please remove the extra M(s)."
+echo "Checking for Vault-Tec misspellings"
+if grep -i 'vault[\s-]?tek' code/**/*.dm; then
+    echo "ERROR: Misspelling(s) of Vault-Tec detected in code, please replace the K with a C."
     st=1
 fi;
-if grep -i 'centcomm' maps/**/*.dmm; then
-    echo "ERROR: Misspelling(s) of CENTCOM detected in maps, please remove the extra M(s)."
+if grep -i 'vault[\s-]?tek' _maps/**/*.dmm; then
+    echo "ERROR: Misspelling(s) of Vault-Tec detected in maps, please replace the K with a C."
     st=1
 fi;
-if grep -ni 'nanotransen' code/**/*.dm; then
-    echo "Misspelling(s) of nanotrasen detected in code, please remove the extra N(s)."
-    st=1
-fi;
-if grep -ni 'nanotransen' maps/**/*.dmm; then
-    echo "Misspelling(s) of nanotrasen detected in maps, please remove the extra N(s)."
-    st=1
-fi;
-if ls maps/*.json | grep -P "[A-Z]"; then
-    echo "Uppercase in a map json detected, these must be all lowercase."
-    st=1
+echo "Checking for light/light_sources in maps"
+if grep -i '^\t(light|light_sources) = ' _maps/**/*.dmm; then
+	echo "ERROR: var/light or var/light_sources set in maps, please remove them."
+	st=1
 fi;
 if grep -i '/obj/effect/mapping_helpers/custom_icon' maps/**/*.dmm; then
     echo "Custom icon helper found. Please include dmis as standard assets instead for built-in maps."
     st=1
 fi;
-for json in maps/*.json
-do
-    map_path=$(jq -r '.map_path' $json)
-    while read map_file; do
-        filename="maps/$map_path/$map_file"
-        if [ ! -f $filename ]
-        then
-            echo "found invalid file reference to $filename in maps/$json"
-            st=1
-        fi
-    done < <(jq -r '[.map_file] | flatten | .[]' $json)
-done
 
 exit $st
